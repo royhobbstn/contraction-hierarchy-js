7ae295896c5b04f6de6c0538564f0c7032ad530f
Daniel Trone <danieljtrone@gmail.com>
2019-03-25 15:24:33 -0500

    finally a reliable dissolve

--- js/build-arc-flags.js
+++ js/build-arc-flags.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs').promises;
 const turf = require('@turf/turf');
+const dissolve = require('geojson-dissolve');
 
 const { clustersKmeans } = require('./turf-kmeans-mod.js');
 
@@ -18,10 +19,14 @@ async function main() {
     if (!feature.geometry.coordinates.length) {
       console.log(index);
     }
-    const start = feature.geometry.coordinates[0].join(',');
+    const start = feature.geometry.coordinates[0]
+      .map(d => d.toFixed(5))
+      .join(',');
     const end = feature.geometry.coordinates[
       feature.geometry.coordinates.length - 1
-    ].join(',');
+    ]
+      .map(d => d.toFixed(5))
+      .join(',');
     pts.add(start);
     pts.add(end);
   });
@@ -50,29 +55,63 @@ async function main() {
     numberOfClusters: 10
   });
 
-  for (let i = 75110; i < 75120; i++) {
-    console.log(clustered.features[i]);
-  }
-
   await fs.writeFile(
     '../arc_flag_output/clustered_points.geojson',
     JSON.stringify(clustered),
     'utf8'
   );
 
-  for (let i = 75110; i < 75120; i++) {
-    console.log(clustered[i]);
-  }
-
-  const voronoi = turf.voronoi(clustered);
-
-  // console.log(voronoi.features[voronoi.features.length - 1]);
-
-  voronoi.features = voronoi.features.slice(75110, 75120);
+  const voronoi = turf.voronoi(pt_feature_collection);
 
   await fs.writeFile(
     '../arc_flag_output/voronoi_regions.geojson',
     JSON.stringify(voronoi),
+    'utf8'
+  );
+
+  const collected = turf.collect(voronoi, clustered, 'cluster', 'cluster');
+
+  // because 'cluster' is an array
+  collected.features.forEach(f => {
+    f.properties.cluster = f.properties.cluster[0];
+  });
+
+  await fs.writeFile(
+    '../arc_flag_output/collected_regions.geojson',
+    JSON.stringify(collected),
+    'utf8'
+  );
+
+  // this is a manual dissolve routine
+  const dissolved_regions = [];
+
+  const dissolved = {};
+
+  collected.features.forEach(feature => {
+    const key = feature.properties.cluster;
+    if (dissolved[key]) {
+      dissolved[key].push(feature);
+    } else {
+      dissolved[key] = [feature];
+    }
+  });
+
+  Object.keys(dissolved).forEach(key => {
+    dissolved_regions.push({
+      type: 'Feature',
+      properties: { region: key },
+      geometry: dissolve(dissolved[key])
+    });
+  });
+
+  const dissolved_regions_collection = {
+    type: 'FeatureCollection',
+    features: dissolved_regions
+  };
+
+  await fs.writeFile(
+    '../arc_flag_output/dissolved_regions.geojson',
+    JSON.stringify(dissolved_regions_collection),
     'utf8'
   );
 }
